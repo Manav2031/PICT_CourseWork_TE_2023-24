7dbdec108d505c2030cbb079a3307f6587fad955
Manav2031 <manavmehta2031@gmail.com>
2024-05-01 16:49:01 +0530

    Updated Assignment 13 of DSBDAL

--- Semester 2/DSBDAL/Assignment 13/Wordcount.scala
+++ Semester 2/DSBDAL/Assignment 13/Wordcount.scala
@@ -1,17 +1,21 @@
 // Load the text file
 val data = sc.textFile("input.txt")
+data.collect
 
 // Split each line into words
 val splitdata = data.flatMap(line => line.split(" "))
+splitdata.collect
 
 // Map each word to a tuple (word, 1)
 val mapdata = splitdata.map(word => (word, 1))
+mapdata.collect
 
 // Reduce by key to get word counts
 val reducedata = mapdata.reduceByKey(_ + _)
-println()
+reducedata.collect
 
+println()
 // Print the word frequencies directly in a clean format
-reducedata.collect().foreach { case (word, count) =>
+reducedata.collect.foreach { case (word, count) =>
   println(s"$word\t$count")
 }
